52ec0d5a470599d82db8094d257626f0174034af
Mike Grigorev <gmause13@yandex.ru>
2026-06-26 17:55:41 +0300

    Update 01_orders_clean.sql

--- sql/01_orders_clean.sql
+++ sql/01_orders_clean.sql
@@ -1,1 +1,10 @@
-
+CREATE OR REPLACE VIEW analytics.orders_clean
+AS SELECT order_id,
+    customer_id,
+    order_status,
+    order_purchase_timestamp[1] AS order_purchase_ts,
+    order_approved_at,
+    order_delivered_carrier_date,
+    order_delivered_customer_date,
+    order_estimated_delivery_date
+   FROM raw.orders;
